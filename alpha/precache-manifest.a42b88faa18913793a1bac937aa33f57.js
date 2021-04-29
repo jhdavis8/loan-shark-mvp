bdd01e24b4b58a7ebf183daaee4838394dad7264
Joshua Davis <jhdavis@udel.edu>
2021-04-29 16:51:11 -0400

    Updates

---
+++ alpha/precache-manifest.a42b88faa18913793a1bac937aa33f57.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4dcdee44cc1d7ed918fc952144d98d2",
+    "url": "/loan-shark-mvp/alphaassets/buttons/button_buy-property.png"
+  },
+  {
+    "revision": "7b57200540426b43446d6c275b1d509a",
+    "url": "/loan-shark-mvp/alphaassets/buttons/button_play.png"
+  },
+  {
+    "revision": "75f40ff160e3e6b8504f7d72213321b5",
+    "url": "/loan-shark-mvp/alphaassets/buttons/button_return-to-town.png"
+  },
+  {
+    "revision": "a76f2a1e53e8a39b00da8aa336ee7b3d",
+    "url": "/loan-shark-mvp/alphaassets/buttons/button_take-loan.png"
+  },
+  {
+    "revision": "340a8e4fa1552719b2d102732ae00112",
+    "url": "/loan-shark-mvp/alphaassets/buttons/sleep_button.png"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/loan-shark-mvp/alphaassets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/loan-shark-mvp/alphaassets/font/font.xml"
+  },
+  {
+    "revision": "700c1998946cf64f89644398f28f14cc",
+    "url": "/loan-shark-mvp/alphaassets/logos/billboard.png"
+  },
+  {
+    "revision": "d62256a0445bdfe24e2e8c3550658be2",
+    "url": "/loan-shark-mvp/alphaassets/logos/loanSharkLogo.png"
+  },
+  {
+    "revision": "95c2545705b559c519133a633db81305",
+    "url": "/loan-shark-mvp/alphaassets/objects/LS-Raft.png"
+  },
+  {
+    "revision": "5306a2fa45d270d11f2d55ea940335eb",
+    "url": "/loan-shark-mvp/alphaassets/objects/UI-ELEMENT-LTextbox.png"
+  },
+  {
+    "revision": "f7bc6e61bef98ff2135f2c342106e89f",
+    "url": "/loan-shark-mvp/alphaassets/objects/UI-ELEMENT-MENU.png"
+  },
+  {
+    "revision": "d1f10f6b2703b013b8cc4c9e5ea069b3",
+    "url": "/loan-shark-mvp/alphaassets/objects/ViableHouse1.png"
+  },
+  {
+    "revision": "1d3acdaeec3fe1650620b5ac2cd31cbf",
+    "url": "/loan-shark-mvp/alphaassets/objects/ViableHouse2.png"
+  },
+  {
+    "revision": "a6e0785de96ebd5c7c35e31a8841b7fb",
+    "url": "/loan-shark-mvp/alphaassets/objects/ViableShop.png"
+  },
+  {
+    "revision": "88261e0abe9bdb980a382b1883de5639",
+    "url": "/loan-shark-mvp/alphaassets/objects/bank.gif"
+  },
+  {
+    "revision": "a40d34e2a8c886f9349ac0f393781382",
+    "url": "/loan-shark-mvp/alphaassets/objects/bank.png"
+  },
+  {
+    "revision": "a3b0ec19e45d8996354a201ec3f71bf8",
+    "url": "/loan-shark-mvp/alphaassets/objects/business.png"
+  },
+  {
+    "revision": "af162e461983f199c07607a7d61dd9dd",
+    "url": "/loan-shark-mvp/alphaassets/objects/businessOld.png"
+  },
+  {
+    "revision": "10cac1d44d22fb9d1c3a52a4ea8eee9d",
+    "url": "/loan-shark-mvp/alphaassets/objects/car.png"
+  },
+  {
+    "revision": "b63b923b011c35f5ed3d150c465415e5",
+    "url": "/loan-shark-mvp/alphaassets/objects/food.png"
+  },
+  {
+    "revision": "e5cafe6bbfd6ad484d633dc5e86711ec",
+    "url": "/loan-shark-mvp/alphaassets/objects/house.png"
+  },
+  {
+    "revision": "45ccfe852bd7f5d83cd9b600ddefe37e",
+    "url": "/loan-shark-mvp/alphaassets/objects/rowboat.png"
+  },
+  {
+    "revision": "f3ce4bd3b18b64e99cefac34563558d0",
+    "url": "/loan-shark-mvp/alphaassets/objects/speedboat.png"
+  },
+  {
+    "revision": "cd450ff14102e00407eb67d1ad556d6f",
+    "url": "/loan-shark-mvp/alphaassets/sprites/Pirate1 (Walk) GIF.gif"
+  },
+  {
+    "revision": "c682b0fe2316ea69e2c1950c1261e5b7",
+    "url": "/loan-shark-mvp/alphaassets/sprites/business.png"
+  },
+  {
+    "revision": "94946769c0a44ea3a861d366b4006f1a",
+    "url": "/loan-shark-mvp/alphaassets/sprites/pirateIdle.png"
+  },
+  {
+    "revision": "a8285779ca6c2eeb2e79facf3b65be06",
+    "url": "/loan-shark-mvp/alphaassets/sprites/pirateWalk.png"
+  },
+  {
+    "revision": "dc707fbe4e11bc957d3c196c7a8b134e",
+    "url": "/loan-shark-mvp/alphaassets/textures/abandoned_house.png"
+  },
+  {
+    "revision": "da7915cdbb2bdd91802c5e3f7b7db87e",
+    "url": "/loan-shark-mvp/alphaassets/textures/beach_background.png"
+  },
+  {
+    "revision": "db526da42ffcd1076b3333c8fe878ef0",
+    "url": "/loan-shark-mvp/alphaassets/textures/dock.png"
+  },
+  {
+    "revision": "1cd92c340d9ecf1bbc197164ac4d741c",
+    "url": "/loan-shark-mvp/alphaassets/textures/dockpillars.png"
+  },
+  {
+    "revision": "3666801242b684e96921ccb07ebb10f8",
+    "url": "/loan-shark-mvp/alphaassets/textures/fishinghut.jpg"
+  },
+  {
+    "revision": "740dbf26b8bddc85c674f0fb45392979",
+    "url": "/loan-shark-mvp/alphaassets/textures/grass.png"
+  },
+  {
+    "revision": "6593b35f914503fcf4fcc3a6d43b2463",
+    "url": "/loan-shark-mvp/alphaassets/textures/grassedge.png"
+  },
+  {
+    "revision": "36e5743640e711891de0a27bd1b2b4b6",
+    "url": "/loan-shark-mvp/alphaassets/textures/mvpgrass.png"
+  },
+  {
+    "revision": "0eeda7ef9beef727ea86e48149918b67",
+    "url": "/loan-shark-mvp/alphaassets/textures/mvpgrassedge.png"
+  },
+  {
+    "revision": "47dcb19ecb23da22a08432243f2f23ad",
+    "url": "/loan-shark-mvp/alphaassets/textures/mvproad.png"
+  },
+  {
+    "revision": "935b1b68f34cca5e39b512ed4b932824",
+    "url": "/loan-shark-mvp/alphaassets/textures/mvpwater.png"
+  },
+  {
+    "revision": "450b2850b66e30225529a797716ef6b1",
+    "url": "/loan-shark-mvp/alphaassets/textures/orgin_beach.gif"
+  },
+  {
+    "revision": "0621f6323808ad66339de9beecb4bd13",
+    "url": "/loan-shark-mvp/alphaassets/textures/road.png"
+  },
+  {
+    "revision": "7e4aee2550100d8ed7a028cce961ed35",
+    "url": "/loan-shark-mvp/alphaassets/textures/roadsand.png"
+  },
+  {
+    "revision": "e8fb457a79fb2d5720cd749a78d0d50d",
+    "url": "/loan-shark-mvp/alphaassets/textures/sand.png"
+  },
+  {
+    "revision": "8f6a22d1c2bb9365703112ad51aecd15",
+    "url": "/loan-shark-mvp/alphaassets/textures/sandedge.png"
+  },
+  {
+    "revision": "5b0cb42d7796f331d2f0f2193174b21a",
+    "url": "/loan-shark-mvp/alphaassets/textures/scroll.png"
+  },
+  {
+    "revision": "614df4561777dc85a73d2b7fc5274ec8",
+    "url": "/loan-shark-mvp/alphaassets/textures/water.png"
+  },
+  {
+    "revision": "90fad952d4f4152a0378",
+    "url": "/loan-shark-mvp/alphagen.main.7bf40e78ea26c05a8413.bundle.js"
+  },
+  {
+    "revision": "f0d0aa5e3ad7570e10f4ef6872dce6ef",
+    "url": "/loan-shark-mvp/alphaindex.html"
+  },
+  {
+    "revision": "b6875d5f4399e4b8a6604f4fc8749a1f",
+    "url": "/loan-shark-mvp/alphamanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/loan-shark-mvp/alphasw.js"
+  }
+]);
